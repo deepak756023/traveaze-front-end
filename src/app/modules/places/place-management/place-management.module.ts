c2742951d5177311e1b98f84e7a09791de816461
deepak-nayak <deepaknayak55260@gmail.com>
2025-02-17 21:53:56 +0530

    removed ng-prime and used bootstrap

--- src/app/modules/places/place-management/place-management.module.ts
+++ src/app/modules/places/place-management/place-management.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule} from '@angular/common';
 import { AllPlacesComponent } from './all-places/all-places.component';
 import { AddPlaceComponent } from './add-place/add-place.component';
 import { UpdatePlaceComponent } from './update-place/update-place.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 
 
@@ -15,7 +16,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+  
   ],
   exports: [
       AddPlaceComponent,
